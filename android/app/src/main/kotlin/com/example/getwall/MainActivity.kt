34f1229b2257dd19c2b171eb97e8b4ddca60aca0
Sugith K <134681105+sugith10@users.noreply.github.com>
2024-06-07 20:00:41 +0530

    Initial commit: Get Wall project using Unsplash API
    - This project fetches cool images from the Unsplash API.

---
+++ android/app/src/main/kotlin/com/example/getwall/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.getwall
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'page/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Get Wall',
+      theme: ThemeData.dark(),
+      home: const HomePage(),
+    );
+  }
+}
